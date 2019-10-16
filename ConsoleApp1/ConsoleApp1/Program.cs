ba6017569de86a2fa21838520ec54986e865fa49
schulzjona <jonajonajona73@gmail.com>
2019-10-16 20:17:03 +0200

    lol

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkInterface[] nics = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+            int i = 0;
+            foreach (NetworkInterface item in nics)
+            {
+                Console.WriteLine("#####################");
+                Console.WriteLine("Index: " + i);
+                Console.WriteLine("Beschreibung: " + item.Description);
+                Console.WriteLine("Name: " + item.Name);
+                IPInterfaceProperties properties = item.GetIPProperties();
+
+                foreach (IPAddressInformation unicast in properties.UnicastAddresses)
+                {
+                    if (unicast.Address.AddressFamily.ToString() == "InterNetworkV6") continue;
+                    Console.WriteLine("IP Adresse: " + unicast.Address);
+                }
+                Console.WriteLine("#####################");
+                Console.WriteLine();
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
