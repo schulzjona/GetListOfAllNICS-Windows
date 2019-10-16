928838a4425e8da7c6025adb61d22a5e7f26fed9
schulzjona <jonajonajona73@gmail.com>
2019-10-16 14:21:38 +0200

    release

---
+++ GetListofAllNICS/GetListofAllNICS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace GetListofAllNICS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NetworkInterface[] nics = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach(NetworkInterface item in nics)
+            {
+                Console.WriteLine();
+                Console.WriteLine("#####################");
+                Console.WriteLine(item.Id);
+                Console.WriteLine(item.Description);
+                Console.WriteLine(item.Name);
+                Console.WriteLine("#####################");
+            }
+        }
+    }
+}
